b077cbb81f022cb29ec453b1b9307bd4a714402f
sapsley <spencer.apsley@gmail.com>
2018-01-18 09:55:51 -0700

    Stubbed out noke device methods

---
+++ nokemobilelibrary/src/main/java/com/noke/nokemobilelibrary/NokeDevice.java
@@ -0,0 +1,87 @@
+package com.noke.nokemobilelibrary;
+
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.util.Log;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Spencer on 1/17/18.
+ */
+
+public class NokeDevice {
+
+    String name;
+    String mac;
+    String serial;
+    String version;
+    String battery;
+
+    public transient byte session[]=new byte[20];
+
+    BluetoothDevice bluetoothDevice;
+    public transient BluetoothGatt gatt;
+    public transient int connectionState;
+    transient int connectionAttempts;
+    transient int rssi;
+
+    private transient JSONArray responses = new JSONArray();
+    protected transient ArrayList<String> commands;
+
+    public transient NokeBluetoothService mService;
+
+    public NokeDevice(String name, String mac){
+
+        this.name = name;
+        this.mac = mac;
+
+        commands = new ArrayList<>();
+        connectionAttempts = 0;
+    }
+
+    public String getVersion(byte[] broadcastBytes, String deviceName) {
+        String version = "";
+        if(deviceName != null) {
+            if(deviceName.contains("NOKE") || deviceName.contains("FOB")) {
+                if(broadcastBytes != null && broadcastBytes.length > 0) {
+                    int majorVersion = broadcastBytes[1];
+                    int minorVersion = broadcastBytes[2];
+                    String hardwareVersion = deviceName.substring(4,6);
+                    return hardwareVersion + "-"+majorVersion+"."+minorVersion;
+                }
+                else {
+                    if(deviceName.contains("2P")) {
+                        return "P2.0";
+                    }
+                    else {
+                        if(deviceName.contains("FOB")) {
+                            return "1F-1.0";
+                        }
+                        else {
+                            return "1P-1.0";
+                        }
+                    }
+                }
+            }
+            else {
+                return "NOT A NOKE DEVICE";
+            }
+        }
+        return version;
+    }
+
+    void setSession(byte[] statusIn) {
+        if(statusIn.length>=20) {
+            System.arraycopy(statusIn, 0, session, 0, 20);
+        }
+    }
+
+
+
+
+
+
+}
