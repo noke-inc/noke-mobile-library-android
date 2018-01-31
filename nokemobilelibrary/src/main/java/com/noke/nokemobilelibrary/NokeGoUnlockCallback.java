9b062d881e2004bf5909b0d2d167ba71d51d2cab
sapsley <spencer.apsley@gmail.com>
2018-01-31 09:54:07 -0700

    Added classes to implement go callback methods

---
+++ nokemobilelibrary/src/main/java/com/noke/nokemobilelibrary/NokeGoUnlockCallback.java
@@ -0,0 +1,19 @@
+package com.noke.nokemobilelibrary;
+
+import nokego.Nokego;
+
+/**
+ * Created by Spencer on 1/31/18.
+ */
+
+public class NokeGoUnlockCallback implements nokego.UnlockCallback {
+    @Override
+    public void receivedUnlockError(String s) {
+
+    }
+
+    @Override
+    public void receivedUnlockResponse(String s) {
+
+    }
+}
---
+++ nokemobilelibrary/src/main/java/com/noke/nokemobilelibrary/NokeGoUploadCallback.java
@@ -0,0 +1,17 @@
+package com.noke.nokemobilelibrary;
+
+/**
+ * Created by Spencer on 1/31/18.
+ */
+
+public class NokeGoUploadCallback implements nokego.UploadCallback {
+    @Override
+    public void receivedUploadError(String s) {
+
+    }
+
+    @Override
+    public void receivedUploadResponse(String s) {
+
+    }
+}
